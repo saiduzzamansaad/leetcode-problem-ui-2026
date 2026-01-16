d531ab738766a98c4294680f6e474c2fc8fc044b
Md saiduzzaman saad <142034434+saiduzzamansaad@users.noreply.github.com>
2026-01-16 19:16:03 +0600

    code add

---
+++  Subrectangle Queries.js
@@ -0,0 +1,18 @@
+class SubrectangleQueries {
+
+      constructor(rectangle) {
+          this.rectangle = rectangle;
+      }
+  
+      updateSubrectangle(row1, col1, row2, col2, newValue) {
+          for (let r = row1; r <= row2; r++) {
+              for (let c = col1; c <= col2; c++) {
+                  this.rectangle[r][c] = newValue;
+              }
+          }
+      }
+  
+      getValue(row, col) {
+          return this.rectangle[row][col];
+      }
+  }
